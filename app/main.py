181719756db8b379331a322eb0bb94eea2aa6b4b
Mohammad Mohiuddin <opummjopu@gmail.com>
2023-04-28 11:11:18 +0200

    Setup project structure

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from app.api.v1.routers import api_router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description="API for aggregating and managing RSS feeds",
+    version="0.1.0",
+    contact={"name": "Mohammad Mohiuddin", "email": "opummjopu@gmail.com"},
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
